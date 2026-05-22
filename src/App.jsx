27c3dba01c8defdb2f8d385a94ddcea096315600
Kushal Vardhan <vardhank140102@gmail.com>
2026-05-22 11:25:23 +0530

    toggle created

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function App() {
+  const [toggle, setToggle] = useState("Stopwatch");
+
+  return (
+    <div className="bg-gray-900 min-h-screen flex justify-center" >
+      <div className="w-60 h-12 bg-white flex p-1 rounded-xl mt-20">
+
+        <div
+          onClick={() => setToggle("Stopwatch")}
+          className={`flex-1 flex items-center justify-center rounded-xl cursor-pointer transition-all duration-200 ${
+            toggle === "Stopwatch"
+              ? "bg-blue-600 text-white"
+              : "bg-white text-black"
+          }`}
+        >
+          Stopwatch
+        </div>
+
+        <div
+          onClick={() => setToggle("Timer")}
+          className={`flex-1 flex items-center justify-center rounded-xl cursor-pointer transition-all duration-200 ${
+            toggle === "Timer"
+              ? "bg-blue-600 text-white"
+              : "bg-white text-black"
+          }`}
+        >
+          Timer
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Stopwatch.jsx
@@ -0,0 +1,3 @@
+import start from "./assets/start.png"
+import pause from "./assets/pause.png"
+import stop from "./assets/stop.png"
