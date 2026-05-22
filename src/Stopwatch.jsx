a541a3732699a32b9586f8c7fe01e38524e80576
Kushal Vardhan <vardhank140102@gmail.com>
2026-05-22 13:32:42 +0530

    stopwatch timer functionality added

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,3 +1,64 @@
-import start from "./assets/start.png"
-import pause from "./assets/pause.png"
-import stop from "./assets/stop.png"
+import start from "./assets/start.png";
+import pause from "./assets/pause.png";
+import stop from "./assets/stop.png";
+import { useEffect, useState } from "react";
+import stopwatch from "./assets/stopwatch.png"
+
+function Stopwatch() {
+  const [startTimer, setStartTimer] = useState(false);
+  const [hour, setHour] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    if (!startTimer) return;
+
+    const interval = setInterval(() => {
+      seconds === 59
+        ? (setSeconds(0),
+          minutes === 59
+            ? (setMinutes(0), setHour((prev) => prev + 1))
+            : setMinutes((prev) => prev + 1))
+        : setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [startTimer, seconds, minutes]);
+  return (
+    <div className="flex justify-center items-center flex-col mt-10 gap-20">
+      <div className="flex ">
+        <div onClick={() => setStartTimer(!startTimer)}>
+          {!startTimer ? (
+            <img src={start} alt="start" className="cursor-pointer" />
+          ) : (
+            <img src={pause} alt="pause" className="cursor-pointer" />
+          )}
+        </div>
+        {startTimer === true ? <img src={stop} alt="stop" /> : ""}
+      </div>
+<div>
+    <img src={stopwatch} alt="stopwatch" />
+</div>
+      <div className="flex gap-15 text-white font-semibold text-3xl">
+        <div>
+          <div>{hour < 10 ? hour.toString().padStart(2, "0") : hour}</div>
+          <div className="text-red-500">Hrs</div>
+        </div>
+        <div>
+          <div>
+            {minutes < 10 ? minutes.toString().padStart(2, "0") : minutes}
+          </div>
+          <div className="text-yellow-500">Min</div>
+        </div>
+        <div>
+          <div>
+            {seconds < 10 ? seconds.toString().padStart(2, "0") : seconds}
+          </div>
+          <div className="text-green-500">Sec</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,3 +1,64 @@
-import start from "./assets/start.png"
-import pause from "./assets/pause.png"
-import stop from "./assets/stop.png"
+import start from "./assets/start.png";
+import pause from "./assets/pause.png";
+import stop from "./assets/stop.png";
+import timer from "./assets/timer.png";
+import { useState } from "react";
+
+function Timer() {
+  const [startTimer, setStartTimer] = useState(false);
+  return (
+    <div className="flex justify-center items-center flex-col mt-10 gap-20">
+      <div className="flex ">
+        <div onClick={() => setStartTimer(!startTimer)}>
+          {!startTimer ? (
+            <img src={start} alt="start" />
+          ) : (
+            <img src={pause} alt="pause" />
+          )}
+        </div>
+        {startTimer === true ? <img src={stop} alt="stop" /> : ""}
+      </div>
+      <div>
+        <img src={timer} alt="timer" />
+      </div>
+      <div className="flex gap-15 text-white font-semibold text-3xl">
+        <div>
+          <input
+            type="number"
+            name=""
+            id=""
+            min={0}
+            max={24}
+            placeholder="00"
+          />
+
+          <div>Hrs</div>
+        </div>
+        <div>
+          <input
+            type="number"
+            name=""
+            id=""
+            min={0}
+            max={60}
+            placeholder="00"
+          />
+          <div>Min</div>
+        </div>
+        <div>
+          <input
+            type="number"
+            name=""
+            id=""
+            min={0}
+            max={60}
+            placeholder="00"
+          />
+          <div>Sec</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
